b351e96f2307a4ca955dbd87d39f50e0a4262ce7
walisson27 <walisson_souza7@hotmail.com>
2025-05-08 09:44:37 -0300

    update Data

--- src/pages/FormularioUser/Data/data.tsx
+++ src/pages/FormularioUser/Data/data.tsx
@@ -8,7 +8,30 @@ const Data = () => {
 
     return (
         <>
-
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="inicio">Data de Início</label>
+                    <input
+                        type="date"
+                        id="inicio"
+                        value={inicio}
+                        onChange={(e) => setInicio(e.target.value)}
+                        className="border border-gray-300 rounded p-2"
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="final">Data de Fim</label>
+                    <input
+                        type="date"
+                        id="final"
+                        value={final}
+                        onChange={(e) => setFinal(e.target.value)}
+                        className="border border-gray-300 rounded p-2"
+                    />
+                </div>
+            </div>
         </>
     );
 }
+
+export default Data;
