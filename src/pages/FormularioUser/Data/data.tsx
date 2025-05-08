f955c4a28a47e44e9f6352f1ea96276f6a55e049
walisson27 <walisson_souza7@hotmail.com>
2025-05-08 09:42:47 -0300

    componente data

---
+++ src/pages/FormularioUser/Data/data.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+
+const Data = () => {
+    const [inicio, setInicio] = useState("");
+    const [final, setFinal] = useState("");
+
+    return (
+        <>
+
+        </>
+    );
+}
