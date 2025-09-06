5a9616d771dd3e21197b681d4f4a6a8d6f7d8cb0
walisson27 <walisson_souza7@hotmail.com>
2025-09-06 14:23:36 -0300

    feat: home news

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -7,16 +7,21 @@ import "../../componentes/navbar/navbar.css"
 import ModeDark from "../Darkmode/ModeDark"
 import Footer from "@/componentes/footer/Footer"
 import { useEffect, useState } from "react"
+import axios from "axios"
 
 
 const Home = () => {
   const [news, setNews] = useState([])
 
-  useEffect(() => {
-    fetch("../api/news")
-      .then(res => res.json())
-      .then(data => setNews(data.articles || []))
-  })
+ useEffect(() => {
+  fetch("../api/news")
+    .then(res => {
+      if (!res.ok) throw new Error("Falha ao carregar notícias");
+      return res.json();
+    })
+    .then(data => setNews(data.articles || []))
+    .catch(err => console.error("Erro ao buscar notícias:", err));
+}, []);
 
     return(
     <DarkModeProvider>
@@ -30,7 +35,13 @@ const Home = () => {
     <div>
       <h1>Últimas notícias de Tecnologia</h1>
       <ul>
-
+        {news.map((dados,index) =>(
+          <li key={index}>
+            <p>{dados.title}</p>
+            <img src={dados.image} alt={dados.name} width={47} height={47} />
+            <strong>{dados.description}</strong> 
+          </li>
+        ))}
         
       </ul>
     </div>
