814c88c248962e155be33b692fde0e6c220be555
walisson27 <walisson_souza7@hotmail.com>
2025-09-13 15:28:11 -0300

    fix: components

--- src/pages/invest/dashboard/Dashboard.tsx
+++ src/pages/invest/dashboard/Dashboard.tsx
@@ -14,10 +14,13 @@ interface Moeda {
 
 const Dashboard = () =>{
   const [dados, setDados] = useState<Moeda[]>([])
+  const [bolsa, setBolsa] = useState([])
   const [pagina, setPagina] = useState(1)
   const [buscar, setBuscar] = useState("")
   const [abriCripto, setAbrirCripto] = useState(false)
   const [modalCripto, setModalCripto] = useState(false)
+  const [abribolsa, setAbribolsa] = useState(false)
+  const [modalbolsa, setModalbolsa] = useState(false)
   const [selectItem, setSelectItem] = useState<Moeda | null >(null)
 
 
@@ -33,6 +36,21 @@ useEffect(() => {
   .then((resposta) => setDados(resposta.data))
   .catch((error) => console.log("Erro ao buscar dados:", error));
 }, [pagina]);
+
+useEffect(() => {
+  axios.get("/api/bolsa", {
+    params: {
+      vs_currency: "brl",
+      order: "market_cap_desc",
+      per_page: 200,
+      page: pagina,
+    },
+  })
+  .then((resposta) => setBolsa(resposta.data))
+  .catch((error) => console.log("Erro ao buscar dados:", error));
+}, [pagina]);
+
+
 
 const filtrados = dados.filter((p) =>
     p.name.toLowerCase().includes(buscar.toLocaleLowerCase())
@@ -59,6 +77,7 @@ console.log(modalCripto)
         <ul>
           <li><input type="text" placeholder="Buscar" name="buscar" value={buscar} onChange={(e)=>setBuscar(e.target.value)}/></li>
           <li><Link href="" onClick={() => setAbrirCripto(prev => !prev)}>{abriCripto ? "Cripto" : "Cripto"}</Link></li>
+          <li><Link href="" onClick={() => setAbribolsa(prev => !prev)}>{abribolsa ? "Bolsa" : "Bolsa"}</Link></li>
           <li><Link href="/invest/invest">Sair</Link></li>
         </ul>
     </nav>
@@ -77,7 +96,7 @@ console.log(modalCripto)
          <div className="modal-overlay" onClick={() => setModalCripto(false)}>
       <div
         className="modal-content"
-        onClick={(e) => e.stopPropagation()} // evita fechar clicando dentro
+        onClick={(e) => e.stopPropagation()} 
       >
         <h2>{selectItem.name}</h2>
         <img src={selectItem.image} alt={selectItem.name} width={92} height={94} />
@@ -99,8 +118,22 @@ console.log(modalCripto)
       )}
         </div>
         )}
+        {abribolsa && (
+          <ul className="ul-news">
+          {bolsa.map((bolsa, index) => (
+            <li className="li-news" key={index}>
+              <div className="stock-card">
+                <h2>{bolsa.shortName} ({bolsa.symbol})</h2>
+                <p><strong>Preço:</strong> R$ {bolsa.regularMarketPrice}</p>
+                <p><strong>Moeda:</strong> {bolsa.currency}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        )}
         {/*<button onClick={() => setPagina((p) => Math.max(1, p - 1))}>Anterior</button>
         <button onClick={() => setPagina((p) =>  p + 1)}>Próxima</button>*/}
+        
     </>
     )
 }
