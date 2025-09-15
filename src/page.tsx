d49400438fd5019f0dd723176abc2650ecbaf5f4
walisson27 <walisson_souza7@hotmail.com>
2025-09-14 21:23:54 -0300

    fix: problem build

--- src/page.tsx
+++ src/page.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { DarkModeProvider } from "./Contexte/Context";
-import ModeDark from "./pages/Darkmode/ModeDark";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../reset.css"
 import "./index.css";
@@ -8,16 +6,13 @@ const App = () => {
   const [mostrarLista, setMostrarLista] = useState(false);
 
   return (
-     <DarkModeProvider>
     <div className="container mt-4">
-      <ModeDark/>
       <h1>Cadastro de Usuários</h1>
       <hr />
       <button className="btn btn-secondary" onClick={() => setMostrarLista(!mostrarLista)}>
         {mostrarLista ? "Ocultar Lista de Usuários" : "Mostrar Lista de Usuários"}
       </button>      
     </div>
-    </DarkModeProvider>
   );
 };
 
