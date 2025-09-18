dd43d597bbf861060c9d524112cacc29548d4334
walisson27 <walisson_souza7@hotmail.com>
2025-09-17 21:18:25 -0300

    fix: padrao do css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,12 @@ import type { AppProps } from "next/app"
 import "../pages/GlobalCss/reset.css"
 import "../pages/GlobalCss/button.css"
 import "../pages/Login-Pagina/login.css"
-
+import "../pages/savings/savings.css"
+import "../pages/navbar/navbar.css"
+import "../pages/invest/Styles/button.css"
+import "../pages/invest/invest.css"
+import "../pages/invest/dashboard/Dashboard.css"
+// import "../pages/invest/dados/dados.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { LoginCenter, LoginBox, Title, Input, Button, LinkCadastro } from "./cadastroStyle"
 //import "../../pages/Login-Pagina/login.css"
 
 interface cadastroUsuario {
@@ -43,15 +42,29 @@ const Cadastro = () => {
   }
 
     return(
-    <LoginCenter>
-      <LoginBox>
-        <Title>Cadastro</Title>
-        <Input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <Input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
-        <Button onClick={cadastro} >Cadastrar</Button>
-        <Button onClick={login} >Login</Button>
-      </LoginBox>
-    </LoginCenter>      
+    <>
+    <div className="login-center">
+      <section className="login">
+        <h1 className="titulo-login">Cadastro</h1>
+        <input
+          className="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="senha"
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        <button onClick={cadastro}>Cadastro</button>
+        <button onClick={login}>Login</button>
+      </section>
+    </div>
+    </>
     )
 
 }
